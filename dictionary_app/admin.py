86f9202f6b02688dc6d0cb536593341904e64d3d
Sanyahu-Designer <sandroaugustovascao@gmail.com>
2024-12-29 20:04:58 -0300

    Atualizado em 29/12/2024 as 20h04

--- dictionary_app/admin.py
+++ dictionary_app/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django import forms
+from django.db import connection
 from .models import GrammaticalCategory, AramaicWord, WordOccurrence, WordCrossReference
 
 @admin.register(GrammaticalCategory)
@@ -17,6 +18,8 @@ class WordCrossReferenceInline(admin.StackedInline):
     model = WordCrossReference
     extra = 1
     fields = ['context']
+    verbose_name = 'Referência Cruzada'
+    verbose_name_plural = 'Referências Cruzadas'
 
 @admin.register(AramaicWord)
 class AramaicWordAdmin(admin.ModelAdmin):
@@ -37,25 +40,37 @@ class AramaicWordAdmin(admin.ModelAdmin):
 
     def save_model(self, request, obj, form, change):
         """Ao salvar a palavra, detecta automaticamente as ocorrências."""
+        # Primeiro salvamos o objeto para garantir que ele exista no banco
         super().save_model(request, obj, form, change)
         
-        # Se for uma nova palavra ou o texto em aramaico foi alterado, detecta ocorrências
+        # Se for uma nova palavra ou o texto em aramaico foi alterado
         if not change or form.changed_data and 'aramaic_word' in form.changed_data:
             from .services import WordOccurrenceService
             from bible_app.models import Verse
+            from django.db import transaction
             
-            # Remove ocorrências antigas
-            WordOccurrence.objects.filter(word=obj).delete()
-            
-            # Detecta e salva novas ocorrências
-            results = WordOccurrenceService.detect_word_occurrences(obj.aramaic_word)
-            for occ in results['occurrences']:
-                try:
-                    verse = Verse.objects.get(
-                        chapter__book__name=occ['book'],
-                        chapter__number=occ['chapter'],
-                        number=occ['verse']
-                    )
-                    WordOccurrence.objects.create(word=obj, verse=verse)
-                except Verse.DoesNotExist:
-                    continue
+            try:
+                with transaction.atomic():
+                    # Remove todas as ocorrências antigas
+                    WordOccurrence.objects.filter(word=obj).delete()
+                    
+                    # Detecta e salva novas ocorrências
+                    results = WordOccurrenceService.detect_word_occurrences(obj.aramaic_word)
+                    for occ in results['occurrences']:
+                        try:
+                            verse = Verse.objects.get(
+                                chapter__book__name=occ['book'],
+                                chapter__number=occ['chapter'],
+                                number=occ['verse']
+                            )
+                            # Verifica se já existe uma ocorrência antes de criar
+                            WordOccurrence.objects.get_or_create(
+                                word=obj,
+                                verse=verse
+                            )
+                        except Verse.DoesNotExist:
+                            continue
+            except Exception as e:
+                # Se houver algum erro, tenta limpar todas as ocorrências
+                WordOccurrence.objects.filter(word=obj).delete()
+                raise e
